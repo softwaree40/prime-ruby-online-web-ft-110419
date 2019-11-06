875f460906c52c2ff5669394b36c5a9aa16497fc
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-06 20:51:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,14 @@
 require 'pry'
-# Add  code here!
-```
+# Add  code here
+def prime?(num)
+ i = 2
+  while i < num 
+    if num % i == 0
+      return false
+     elsif num < 0
+       i+=1
+     return false
+    end
+  end
+   return true 
+end
