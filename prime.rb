63c1c59e198b9f03aeeec065999264c85c01a237
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-06 21:07:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+require 'pry'
+# Add  code here
+def prime?(num)
+   if num < 2 
+     return false 
+   end
+    (2...num).each do |divisor|
+      if num % divisor == 0 
+        return false 
+      end
+    end
+    return true 
+end
